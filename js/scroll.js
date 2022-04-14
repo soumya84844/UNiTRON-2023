3a2bb40fcacb59b25cc13c201729e9765848e3e0
soumya <soumyadeep.behala2001@gmail.com>
2022-04-14 13:12:11 +0530

    Minor fixes

--- js/scroll.js
+++ js/scroll.js
@@ -1,6 +1,7 @@
 function follow(item) {
   let href=item.href;
-  let id=href.slice(50);
+  console.log(href);
+  let id=href.slice(-5);
   console.log(id);
   document.getElementById(id).scrollIntoView({behavior: "smooth", block: "center", inline: "nearest"});
 }
