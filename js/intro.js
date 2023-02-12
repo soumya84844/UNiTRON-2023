fc8a5ce7347d7a6e8fed671354e5fe312d2a4bf6
soumya <soumyadeep.behala2001@gmail.com>
2023-02-12 17:05:56 +0530

    Minor Fixes

--- js/intro.js
+++ js/intro.js
@@ -1,43 +1,4 @@
-// Create and reference video element
-var vid = document.createElement('video');
-
-// Add class for styling
-vid.classList.add('playing');
-
-// Add a src to .vid
-vid.src = 'videos/intro-vid.mp4';
-
-// Load .vid
-vid.load();
-
-// Add .vid to body
-document.body.appendChild(vid);
-
-vid.muted=true;
-
-// Play video
-vid.play();
-
-/* Register ended event to vid
-|| After video has ended...
-*/
-vid.addEventListener('ended', function(e) {
-
-  // Pause vid
-  vid.pause();
-
-  /* Reset time played. This method used
-  || along with .pause() is equivelant to "stop"
-  */
-  vid.currentTime = 0;
-
-  // Simulate a `non-playing state`
-  vid.classList.remove('playing');
-
-  /* Delay the call to remove vid in order
-  || to preserve the fade ouyt effect.
-  */
-  setTimeout(function() {
-    document.body.removeChild(vid);
-  }, 2000);
-}, false);
+$window.on('load', function() {
+  $(".loader").fadeOut(10000);
+  $(".content").fadeIn(10000);
+});
