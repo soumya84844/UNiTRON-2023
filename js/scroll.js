48ffc70c2eee73e10cbcb89fcdf98c84ae7630d8
soumya <soumyadeep.behala2001@gmail.com>
2023-02-18 15:58:48 +0530

    Added team photo layout

--- js/scroll.js
+++ js/scroll.js
@@ -1,7 +1,7 @@
-function follow(item) {
-  let href=item.href;
-  console.log(href);
-  let id=href.slice(-5);
-  console.log(id);
-  document.getElementById(id).scrollIntoView({behavior: "smooth", block: "center", inline: "nearest"});
-}
+// function follow(item) {
+//   let href=item.href;
+//   console.log(href);
+//   let id=href.slice(-5);
+//   console.log(id);
+//   document.getElementById(id).scrollIntoView({behavior: "smooth", block: "center", inline: "nearest"});
+// }
